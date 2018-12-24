7bb91192df0b55bf5f209317f7691ec000429871
Rafał Prońko <rafal.pronko@ynd-consult.com>
2018-12-24 08:55:58 +0100

    add functions script to the project with useful functions

--- README.md
+++ README.md
@@ -3,28 +3,31 @@
 ## Methods
 
 ### Genetic Algorithsm
+
 1. Notebook (80% ready)
 
 ### Nearest neighbour algorithm
+
 1. Notebook (50% ready)
 
 ### Anneal
+
 1. Notebook (70% ready)
 
 ### 2-opt
-1. Notebook (80% ready)
 
+1. Notebook (80% ready)
 
 ### 3-opt
 
-
 ### Ant colony
+
 1. Notebook (0%)
 2. Bibliography
     1. [Wiki](https://en.wikipedia.org/wiki/Ant_colony_optimization_algorithms)
-   
 
 ### Bee colony
+
 1. Notebook (0%)
 2. Bibliography
     1. [wiki](https://en.wikipedia.org/wiki/Artificial_bee_colony_algorithm)
@@ -35,20 +38,23 @@
 ### Tabu search
 
 ### Cross-entropy methods
+
 1. Notebook (0%)
 2. Bibliography
     1. [Tutorial of CE methods](http://web.mit.edu/6.454/www/www_fall_2003/gew/CEtutorial.pdf)
 
 ### Markov chain
+
 1. Notebook (0%)
 2. Bibliography
     1. [Large-Step Markov Chains for the Traveling Salesman Problem...](http://www.cs.ubc.ca/labs/beta/Courses/CPSC532D-03/Resources/MarOttFel91.pdf)
 
+### Representation of TSP
 
-### Representation of TSP!
 1. [Splay trees](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.49.570&rep=rep1&type=pdf)
     1. [Splay tree original paper](http://www.cs.cmu.edu/%7Esleator/papers/self-adjusting.pdf)
     2. [Splay tree lecture](https://cglab.ca/~morin/teaching/5408/notes/splay.pdf)
 
 ## Bibliography
+
 1. [TSP different approach](https://www2.seas.gwu.edu/~simhaweb/champalg/tsp/tsp.html)
---
+++ scripts/functions.py
@@ -0,0 +1,72 @@
+from numba import njit, prange
+from sympy import primerange
+import numpy as np
+
+
+@njit(parallel=True)
+def score_tour_numba(tour_, X, Y, primes):
+    """Score for path. X and Y in the node id order.
+
+    Arguments:
+        tour_ {numpy array} -- path
+        X {numpy array} -- X coordynate for the node
+        Y {numpy array} -- Y coordynate for the node
+        primes {list} -- list of primes town
+
+    Returns:
+        float -- the score of the path.
+    """
+
+    full = 0.0
+    for i in prange(0, len(tour_)-1):
+        alpha = 1.0
+        dst = np.hypot(X[tour_[i]] - X[tour_[i+1]],
+                       Y[tour_[i]] - Y[tour_[i+1]])
+        if i % 10 == 9 and primes[tour_[i]] == 0:
+            alpha = 1.1
+        full += alpha * dst
+    return full
+
+
+def generate_primes(max_number):
+    return list(primerange(0, max_number))
+
+
+@njit
+def reverse_subpath(path, i, k):
+    """Create the reverse subpath.
+
+    Arguments:
+        path {numpy array} -- Path 
+        i {int} -- id node to start from
+        k {int} -- id node to finish
+
+    Returns:
+        numpy array -- new path
+    """
+
+    while i < k:
+        path[i], path[k] = path[k], path[i]
+        i += 1
+        k -= 1
+    return path
+
+
+def local2opt_swap(path, i, k):
+    path[i], path[k] = path[k], path[i]
+    return path
+
+
+@njit
+def local2opt_swap_numba(path, i, k):
+    """Make a swap between two nodes in the path in numba.
+
+    Args:
+      path: list of the nodes
+      i: int first node to swap
+      k: int second node to swap
+    Returns:
+      New path.
+    """
+    path[i], path[k] = path[k], path[i]
+    return path
